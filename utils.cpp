7ccfea6840e4d5ae8f54398c25c6156be1b06384
Maximilian Grau <Maximilian.Grau@st.ovgu.de>
2018-03-24 18:10:11 +0100

    add nop

--- utils.cpp
+++ utils.cpp
@@ -2,18 +2,20 @@
 
 void wait_us(uint32_t duration)
 {
-	uint32_t lastTime = micros();
-	while (micros() - lastTime < duration)
-	{
-		// wait
-	}
+  uint32_t lastTime = micros();
+  while (micros() - lastTime < duration)
+  {
+    asm("nop");
+    // wait
+  }
 }
 
 void wait_ms(uint32_t duration)
 {
-	uint32_t lastTime = millis();
-	while (millis() - lastTime < duration)
-	{
-		// wait
-	}
+  uint32_t lastTime = millis();
+  while (millis() - lastTime < duration)
+  {
+    asm("nop");
+    // wait
+  }
 }
